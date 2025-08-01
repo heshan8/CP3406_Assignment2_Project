1f3f8d9c6d39e9b842d158216939859bb94c0ebf
heshan8 <heshan988@gmail.com>
2025-08-01 19:29:42 +1000

    Upgrade Room to version 2.7.2 and refactor dependencies to use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
