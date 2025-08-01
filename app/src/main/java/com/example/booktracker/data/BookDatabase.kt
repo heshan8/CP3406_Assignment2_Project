c7e42943619903b1b0b2154080f17862cb187792
heshan8 <heshan988@gmail.com>
2025-08-01 21:33:39 +1000

    Add required imports

--- app/src/main/java/com/example/booktracker/data/BookDatabase.kt
+++ app/src/main/java/com/example/booktracker/data/BookDatabase.kt
@@ -1,2 +1,8 @@
 package com.example.booktracker.data
 
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import android.content.Context
+
